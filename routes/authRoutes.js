264b4958f5dbf42ba6318a11a36b6be3f3ab8213
scalzone <scalzone@pdx.edu>
2023-07-26 17:11:50 -0700

    updated passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,7 @@ module.exports = app => {
     app.get('/auth/google/callback',
      passport.authenticate('google'),
      (req, res) => {
-        res.redirect(/"surveys");
+        res.redirect("/surveys");
      }
      );
 
